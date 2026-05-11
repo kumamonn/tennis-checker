727f6cc40ce9bdcb082282fe8c5d54761af54c99
shoma hibio <a_bathing_ape0o0@yahoo.co.jp>
2026-05-11 10:26:59 +0900

    改修

--- src/tennis/bin/Load-Env.ps1
+++ src/tennis/bin/Load-Env.ps1
@@ -1,7 +1,23 @@
-Get-Content .env | ForEach-Object {
+param(
+    [Parameter(Mandatory)]
+    [string]$BaseDir
+)
+
+$envFile = Join-Path $BaseDir ".env"
+
+if (-not (Test-Path $envFile)) {
+    throw ".env file not found: $envFile"
+}
+
+Get-Content $envFile | ForEach-Object {
+
+    # コメント行
     if ($_ -match '^\s*#') { return }
+
+    # 空行
     if ($_ -match '^\s*$') { return }
 
     $name, $value = $_ -split '=', 2
-    [System.Environment]::SetEnvironmentVariable($name, $value)
+
+    [System.Environment]::SetEnvironmentVariable($name.Trim(), $value.Trim())
 }
--- src/tennis/bin/katsushika_tennis_holiday.ps1
+++ src/tennis/bin/katsushika_tennis_holiday.ps1
@@ -11,8 +11,9 @@ $BASE_DIR = $PSScriptRoot
 $FileName = $FileName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Path)
 #$LOGF    = "${AplPath}\log\${FileName}.log"
 
+(Join-Path $BASE_DIR ".env")
 if (Test-Path (Join-Path $BASE_DIR ".env")) {
-    .(Join-Path $BASE_DIR "Load-Env.ps1")
+    .(Join-Path $BASE_DIR "Load-Env.ps1") -BaseDir $BASE_DIR
 }
 
 .(Join-Path $BASE_DIR "KTSK_ENV.ps1")
@@ -53,53 +54,59 @@ try{
         foreach ( $holiday in $holidays ){
         
             "対象日付：" + ${holiday}.ToString("yyyy-MM-dd")
-        
+            
             $y = $holiday.ToString("yyyy")
             $m = $holiday.ToString("MM")
             $d = $holiday.ToString("dd")
             
-            $URL = "javascript:selectDay((_dom == 3) ? document.layers['disp'].document.form1 : document.form1, gRsvWInstSrchVacantWAllAction, 1, ${y}, ${m}, ${d})"
-            $driver.ExecuteScript($URL)
-            Start-Random-Sleep
+            try{
             
-            #imgタグ内にある空きマーク画像の数を数える
-            #$emptyImgs = 0
+                $URL = "javascript:selectDay((_dom == 3) ? document.layers['disp'].document.form1 : document.form1, gRsvWInstSrchVacantWAllAction, 1, ${y}, ${m}, ${d})"
+                $driver.ExecuteScript($URL)
+                Start-Random-Sleep
+                
+                #imgタグ内にある空きマーク画像の数を数える
+                #$emptyImgs = 0
 
 
-            #空き状況画面のTR要素（YYYY年MM月DD日、テニスコートA面、テニスコートB面...etc）
-            $TR_ELEMENTS = $driver.FindElements([By]::XPath("//*[@id='disp']/center/form/center/table/tbody/tr[4]/td/table/tbody/tr/td[1]/table/tbody/tr"))
-            #$TR_ELEMENTS = $TR_ELEMENTS[1,2,3,4,5,6,7,8,9] #1行目はヘッダのためカット
-            $TR_HEADER = $TR_ELEMENTS[0]
-            
-            $TR_ELEMENTS = $TR_ELEMENTS[1 .. $TR_ELEMENTS.Count]
-            #"TR_ELEMENTS=" + $TR_ELEMENTS.Count
-            foreach($TR in $TR_ELEMENTS){
-                $TD_ELEMENTS = $TR.FindElements([By]::TagName("TD"))
-                #"TD_ELEMENTS=" + $TD_ELEMENTS.Count
-                $imgTags = $TD_ELEMENTS.FindElements([By]::TagName("img"))
-                for ($i=0; $i -lt $imgTags.Count; $i++){
-                #$imgTags | %{
+                #空き状況画面のTR要素（YYYY年MM月DD日、テニスコートA面、テニスコートB面...etc）
+                $TR_ELEMENTS = $driver.FindElements([By]::XPath("//*[@id='disp']/center/form/center/table/tbody/tr[4]/td/table/tbody/tr/td[1]/table/tbody/tr"))
+                #$TR_ELEMENTS = $TR_ELEMENTS[1,2,3,4,5,6,7,8,9] #1行目はヘッダのためカット
+                $TR_HEADER = $TR_ELEMENTS[0]
+                
+                $TR_ELEMENTS = $TR_ELEMENTS[1 .. $TR_ELEMENTS.Count]
+                #"TR_ELEMENTS=" + $TR_ELEMENTS.Count
+                foreach($TR in $TR_ELEMENTS){
+                    $TD_ELEMENTS = $TR.FindElements([By]::TagName("TD"))
+                    #"TD_ELEMENTS=" + $TD_ELEMENTS.Count
+                    $imgTags = $TD_ELEMENTS.FindElements([By]::TagName("img"))
+                    for ($i=0; $i -lt $imgTags.Count; $i++){
+                    #$imgTags | %{
 
-                    if($imgTags[$i].GetAttribute("src").Contains("lw_emptybs.gif") -eq "True"){
-                        #$emptyImgs = $emptyImgs + 1 
+                        if($imgTags[$i].GetAttribute("src").Contains("lw_emptybs.gif") -eq "True"){
+                            #$emptyImgs = $emptyImgs + 1 
 
-                        $TH_HEADERS = $TR_HEADER.FindElements([By]::TagName("TH"))
-                        $TH_HEADERS = $TH_HEADERS[1..$TH_HEADERS.Count] #ヘッダの1列目はYYYY年MM月DD日）
+                            $TH_HEADERS = $TR_HEADER.FindElements([By]::TagName("TH"))
+                            $TH_HEADERS = $TH_HEADERS[1..$TH_HEADERS.Count] #ヘッダの1列目はYYYY年MM月DD日）
 
-                        $coat = createCoatObject $CoatKey $holiday $TH_HEADERS[$i] $TR
-                        $coat
-                        $emptyCoats.Add($coat) 
+                            $coat = createCoatObject $CoatKey $holiday $TH_HEADERS[$i] $TR
+                            $coat
+                            $emptyCoats.Add($coat) 
 
+                        }
                     }
-                }
 
-            }    
+                }
             
-            #調べ終わったら元のカレンダー画面に戻ります
-            $URL = "javascript:doAction((_dom == 3) ? document.layers['disp'].document.form1 : document.form1, gRsvWInstSrchVacantBackWAllAction)"
-            $driver.ExecuteScript($URL)
-            Start-Random-Sleep
-                    
+            } catch [Exception] {
+                 $Error[0]
+            } finally {
+                #調べ終わったら元のカレンダー画面に戻ります
+                $URL = "javascript:doAction((_dom == 3) ? document.layers['disp'].document.form1 : document.form1, gRsvWInstSrchVacantBackWAllAction)"
+                $driver.ExecuteScript($URL)
+                Start-Random-Sleep
+            }
+            
         }
         
         #コート選択画面に戻る
@@ -143,7 +150,7 @@ $BODY_TAMPLATE = @"
 ${BodyCoat}
 
 ■葛飾区公共施設予約システム
-https://rsv.shisetsu.city.katsushika.lg.jp/web/menu.jsp
+https://rsv.shisetsu.city.katsushika.lg.jp/katsushika/web/index.jsp
 (10009312/1231)
 "@
 
